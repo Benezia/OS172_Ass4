a8594bbd34b900790992b8a269d1383bc2de46af
Benezia <ben.venezi@gmail.com>
2017-06-07 21:08:26 +0300

    Fixed minor bug in mkfs.c from the BGU cloned project

--- mkfs.c
+++ mkfs.c
@@ -11,8 +11,9 @@
 #include "stat.h"
 #include "param.h"
 
+#ifndef static_assert
 #define static_assert(a, b) do { switch (0) case 0: case (a): ; } while (0)
-
+#endif
 int nblocks = (995-LOGSIZE);
 int nlog = LOGSIZE;
 int ninodes = 200;
